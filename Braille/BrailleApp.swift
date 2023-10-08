cec5fccb2d7d0216596ce8cd85cb9efc8a7e47d8
Eliana Ecuba <elianaamh7@gmail.com>
2023-10-08 10:27:03 +0200

    Initial Commit

---
+++ Braille/BrailleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrailleApp.swift
+//  Braille
+//
+//  Created by Eliana Ecuba on 08/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BrailleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
